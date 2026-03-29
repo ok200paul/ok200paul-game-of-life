3d386746258b83f115afcadfee787b69c9d2f6d7
Paul Grimes <paul@ok200.net>
2026-03-29 20:11:01 +1100

    Initial application

---
+++ wasm_game_of_life.d.ts
@@ -0,0 +1,73 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+export class Universe {
+    private constructor();
+    free(): void;
+    [Symbol.dispose](): void;
+    cells(): number;
+    height(): number;
+    static new(): Universe;
+    /**
+     * Set the height of the universe.
+     *
+     * Resets all cells to the dead state.
+     */
+    set_height(height: number): void;
+    /**
+     * Set the width of the universe.
+     *
+     * Resets all cells to the dead state.
+     */
+    set_width(width: number): void;
+    tick(): void;
+    toggle_cell(row: number, column: number): void;
+    width(): number;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly __wbg_universe_free: (a: number, b: number) => void;
+    readonly universe_cells: (a: number) => number;
+    readonly universe_height: (a: number) => number;
+    readonly universe_new: () => number;
+    readonly universe_set_height: (a: number, b: number) => void;
+    readonly universe_set_width: (a: number, b: number) => void;
+    readonly universe_tick: (a: number) => void;
+    readonly universe_toggle_cell: (a: number, b: number, c: number) => void;
+    readonly universe_width: (a: number) => number;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ wasm_game_of_life_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_universe_free: (a: number, b: number) => void;
+export const universe_cells: (a: number) => number;
+export const universe_height: (a: number) => number;
+export const universe_new: () => number;
+export const universe_set_height: (a: number, b: number) => void;
+export const universe_set_width: (a: number, b: number) => void;
+export const universe_tick: (a: number) => void;
+export const universe_toggle_cell: (a: number, b: number, c: number) => void;
+export const universe_width: (a: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
